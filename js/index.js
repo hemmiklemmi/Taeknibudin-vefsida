141345c50905183984d87ab99f0325d3a45b45ed
hemmiklemmi <hemmioskar@gmail.com>
2021-10-20 16:10:29 +0000

    lagaði header og footer og bætti við dropdownmenu a hinar siðurnar

--- js/index.js
+++ js/index.js
@@ -4,3 +4,12 @@ function drop_down(){
    document.getElementById("frontpage-header").classList.toggle("hidebackground");
    document.getElementById("body").classList.toggle("overflow");
 }
+function drop_down_2(){
+    document.getElementById("menu-mobile").classList.toggle("show");
+    document.getElementById("below-menu").classList.toggle("hide");
+    document.getElementById("body").classList.toggle("overflow");
+ }
+ function drop_down_3(){
+    document.getElementById("menu-mobile").classList.toggle("show");
+    document.getElementById("body").classList.toggle("overflow");
+ } 
