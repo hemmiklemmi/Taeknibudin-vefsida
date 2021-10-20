b5cd76e4241f2b4c806fd3174e94e087e3eb583b
hemmiklemmi <hemmioskar@gmail.com>
2021-10-20 15:24:32 +0000

    vann i dropdownmenu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 function drop_down(){
-    document.getElementById("menu-mobile").style.display("block");
+   document.getElementById("menu-mobile").classList.toggle("show");
+   document.getElementById("below-menu").classList.toggle("hide");
+   document.getElementById("frontpage-header").classList.toggle("hidebackground");
+   document.getElementById("body").classList.toggle("overflow");
 }
